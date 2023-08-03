5e1dc9bf306279a5c0d5d5b3e1ee98afed84b7d8
陈东 <471088280@qq.com>
2023-08-03 19:37:05 +0800

    --story=instance_node

--- src/instance_node.cc
+++ src/instance_node.cc
@@ -2,5 +2,11 @@
 
 namespace epaxos{
 
+ResCode Instance::InitInsFromString(std::string &body){
+    std::cout<<"before: "<< ins_.DebugString()<<std::endl;
+    bool t = ins_.ParseFromString(body);
+    std::cout<<"localind: "<< ins_.DebugString()<<std::endl;
+    return ResCode(t);
+}
 
 };
--- src/instance_node.h
+++ src/instance_node.h
@@ -2,19 +2,20 @@
 #define __EPAXOS_INSTANCE_NODE_H__
 
 #include "proto/instance.pb.h"
-#include "include/"
+#include "include/rescode.h"
 
 namespace epaxos{
 
 class Instance{
 public:
     Instance(){}    //从db中直接解析，或者直接拿到id
+    Instance(epxos_instance_proto::EpInstance & t):ins_(t){}
 
     ResCode InitInsFromString(std::string &body);
 
-    ResCode step(epxos_instance_proto::EpInstance *ins);
+    ResCode step(epxos_instance_proto::InstanceSwapMsg *ins);
 
-    ResCode step_ack(const epxos_instance_proto::EpInstance & ins);
+    ResCode step_ack(const epxos_instance_proto::InstanceSwapMsg & ins);
 
 private:
     ResCode PreAccept();
